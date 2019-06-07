ed6b2d66b965051d6692d7120ae73c9425c96665
VladimirAmiorkov <vladimir.amiorkov@progress.com>
2019-06-07 17:40:17 +0300

    fix: add component wide 'className' proeprty that is exposing the `nativeElement` property #27

--- src/angular/nativescript-picker.directives.ts
+++ src/angular/nativescript-picker.directives.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, ElementRef, forwardRef, IterableDiffers } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ElementRef, forwardRef, IterableDiffers, AfterContentInit, OnDestroy } from "@angular/core";
 import { TemplatedItemsComponent, TEMPLATED_ITEMS_COMPONENT } from "nativescript-angular/directives/templated-items-comp";
 import { PickerField } from "../picker";
 import { PickerValueAccessor } from "./nativescript-picker.accessors";
+import { View } from "tns-core-modules/ui/core/view";
 
 @Component({
     selector: "PickerField",
@@ -12,9 +13,15 @@ import { PickerValueAccessor } from "./nativescript-picker.accessors";
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [{ provide: TEMPLATED_ITEMS_COMPONENT, useExisting: forwardRef(() => PickerFieldComponent) }]
 })
-export class PickerFieldComponent extends TemplatedItemsComponent {
+export class PickerFieldComponent extends TemplatedItemsComponent implements AfterContentInit {
+    private _className: string;
+
     public get nativeElement(): PickerField {
         return this.templatedItemsView;
+    }
+
+    public get className(): string {
+        return this._className;
     }
 
     protected templatedItemsView: PickerField;
@@ -22,6 +29,23 @@ export class PickerFieldComponent extends TemplatedItemsComponent {
     constructor(_elementRef: ElementRef,
         _iterableDiffers: IterableDiffers) {
         super(_elementRef, _iterableDiffers);
+    }
+
+    ngAfterContentInit() {
+        super.ngAfterContentInit();
+        this.nativeElement.on("classNameChange", this.onClassNameChange.bind(this));
+    }
+
+    ngOnDestroy() {
+        if (this.nativeElement) {
+            this.nativeElement.off("classNameChange", this.onClassNameChange.bind(this));
+        }
+
+        super.ngOnDestroy();
+    }
+
+    private onClassNameChange(args) {
+        this. _className = (<View>args.object).className;
     }
 }
 
